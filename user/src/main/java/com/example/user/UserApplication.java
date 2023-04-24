9a29a9ab28dba0b1333b373640de0cf70b88f266
nowgnas <swlee0376@gmail.com>
2023-04-24 17:22:31 +0900

    :card_file_box: amend file

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -34,4 +34,18 @@ public class UserApplication {
         return "Configuration File's Message Owner: " + messageOwner + "\n"
                 + "Configuration File's Message Content: " + messageContent;
     }
+
+
+    @GetMapping("/config/database")
+    public String database(@Value("${spring.datasource.driver}") String driver,
+                           @Value("${spring.datasource.url}") String url,
+                           @Value("${spring.datasource.username}") String username,
+                           @Value("${spring.datasource.password}") String password,
+                           @Value("${token.key}") String tokenKey) {
+        return "driver: " + driver + "\n"
+                + "url: " + url + "\n"
+                + "username: " + username + "\n"
+                + "password: " + password + "\n\n"
+                + "token key: " + tokenKey;
+    }
 }
