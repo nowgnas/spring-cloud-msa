96424ed4c2877abcb34f8ef3d05d7dba16f3d3ce
nowgnas <swlee0376@gmail.com>
2023-04-24 16:21:19 +0900

    :sparkles: add config server

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.example.user;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -25,5 +26,12 @@ public class UserApplication {
     @GetMapping("/auth")
     public String auth(@RequestHeader(value = "token") String token) {
         return "token is " + token;
+    }
+
+    @GetMapping("/config")
+    public String string(@Value("${message.owner}") String messageOwner,
+                         @Value("${message.content}") String messageContent) {
+        return "Configuration File's Message Owner: " + messageOwner + "\n"
+                + "Configuration File's Message Content: " + messageContent;
     }
 }
