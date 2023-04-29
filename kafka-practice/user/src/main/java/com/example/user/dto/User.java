af58f44b5873f8b2377f18641b134fda2de8a368
nowgnas <dev.nowgnas@gmail.com>
2023-04-29 15:27:47 +0900

    :memo: add docker shell script

--- kafka-practice/user/src/main/java/com/example/user/dto/User.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.dto;
-
-import lombok.Data;
-
-@Data
-public class User {
-    private String name;
-}
--- kafka-practice/user/src/main/java/com/example/user/dto/UserDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.user.dto;
-
-public class UserDto {
-}
